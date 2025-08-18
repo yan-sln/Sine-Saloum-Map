7a0d6a0eb5d020dc290ca90e0ca99dc4a638d61d
Toppics <110732997+yan-sln@users.noreply.github.com>
2025-08-18 23:19:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,205 +2,161 @@
 """
 Created on Fri Aug  5 09:44:49 2022
 
+Main "global variables" class for downloading SentinelHub data
+and managing plotting parameters for KML/CSV overlay.
+
 @author: yan-s
 """
 
 import os
 import logging
+from typing import List, Tuple
 
 
 class VariableGlobal:
-    """Download satellite map from sentinelhub & plot data from googleEarth .kml file on it.
+    """Download satellite map from SentinelHub & plot data from GoogleEarth .kml file.
 
-What it does :    
-Script downloading with sentinelhub api map in color with minimal cloud exposure of a box (not too large) input.
-Transform a .kml file from GoogleEarth into a .csv file with name and coordinates.
-Finally, plot name with coordonates on the sentinelhub map download.
-    
-How it does:
-    using https://docs.sentinel-hub.com/api/latest/
+    What it does:
+        - Download SentinelHub satellite map imagery.
+        - Convert .kml file from GoogleEarth into a .csv file with names and coordinates.
+        - Plot names with coordinates on the SentinelHub map download.
 
-Libraries needed :
-    os
-    cv2
-    csv
-    time
-    utils
-    pandas
-    logging
-    matplotlib
-    sentinelhub (https://sentinelhub-py.readthedocs.io/en/latest/install.html)
-    
-Minimal input:
-    -Sentinelhub Credentials ; Get credentials at https://www.sentinel-hub.com/ .
-    -A path to .kml file created with GoogleEarth ; https://earth.google.com/web/ .
-    or a csv file with at least 3 columns untitled 'Name', 'Longitude' & 'Latitude' (case sensitive) and using wgs84 coordinate system.
-    -A Boundary Box corresponding (not too large) using wgs84 coordinate system ; https://geojson.io/#map=2/20.0/0.0 .
-    
-Return/create :
-    -Directory called "SentinelDownload" containing :
-        -A finale .png file.
-        -Possibly a .csv file.
-        -Subdirectories from sentinelhub containing .json, .tiff & .png file.
+    Requirements:
+        - SentinelHub credentials (https://www.sentinel-hub.com/).
+        - .kml file exported from GoogleEarth or .csv with Name/Longitude/Latitude columns.
+        - WGS84 coordinate system for inputs.
 
-Way of improvment :
-    -System to import shapefile https://gis.stackexchange.com/a/396133 .
-    -Function for calculate if coordinates outside of BBox.
-    -Better documentation.
-    -Customisation option.
+    Output:
+        - Directory "SentinelDownload" containing:
+            - Final .png file.
+            - Optional .csv file.
+            - Subdirectories with .json, .tiff & .png files from SentinelHub.
 
-Note :
-    Use wgs84 coordinate system.
-    Choose BoundaryBox not too large.
-    Personnal .csv file MUST have column entitled 'Name', 'Longitude' & 'Latitude' (Case sensitive).
-    Add 'Name' in :
-        -self.listyDown for annotate 'Name' underneath the point (could leave empty)
-        -self.listyUp for annotate 'Name' above the point.
-    If 'Name' are UPPER, will annotate them in color.
-    For maximum ease in customisation, all variables are in the main script (here).
+    Possible improvements:
+        - Add shapefile support.
+        - Add function to check if coordinates are outside BBox.
+        - Improve documentation and customization options.
     """
 
-    def __init__(self):
-        """Basically control inputs of all scripts w/ pseudo-global variables"""
-        
-        "Logging()"
-        # Set format for logging outputs
-        logging.basicConfig(level=logging.DEBUG,
-                            format='[%(levelname)s] %(asctime)s - %(message)s')
-        # Block unwanted imported module logging
-        logging.getLogger('PIL').setLevel(logging.WARNING)
-        logging.getLogger('requests').setLevel(logging.WARNING)
-        logging.getLogger('urllib3').setLevel(logging.WARNING)
-        logging.getLogger('sentinelhub').setLevel(logging.WARNING)
-        logging.getLogger('http.client').setLevel(logging.WARNING)
-        logging.getLogger('matplotlib.font_manager').setLevel(logging.ERROR)
-        logging.getLogger('requests.packages.urllib3').setLevel(logging.WARNING)
+    def __init__(self) -> None:
+        """Initialize pseudo-global variables and manage working directory."""
 
-        "SentinelHubDownload()"
-        
-        # Get credentials at https://www.sentinel-hub.com/
-        self.instanceID = ''
-        self.clientID = ''
-        self.clientSecret = ''
+        # Logging setup
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format="[%(levelname)s] %(asctime)s - %(message)s",
+        )
+        logging.getLogger("PIL").setLevel(logging.WARNING)
+        logging.getLogger("requests").setLevel(logging.WARNING)
+        logging.getLogger("urllib3").setLevel(logging.WARNING)
+        logging.getLogger("sentinelhub").setLevel(logging.WARNING)
+        logging.getLogger("http.client").setLevel(logging.WARNING)
+        logging.getLogger("matplotlib.font_manager").setLevel(logging.ERROR)
+        logging.getLogger("requests.packages.urllib3").setLevel(logging.WARNING)
 
-        # https://geojson.io/#map=2/20.0/0.0
-        # Provide BBox with wgs84 coordinate system
-        self.BBox = [-16.900000, 13.600000, -16.300000, 14.200000]
+        # SentinelHub credentials
+        self.instanceID: str = ""
+        self.clientID: str = ""
+        self.clientSecret: str = ""
 
-        # Ratio meters/pixel (With this Sat Bands, 30m is max)
-        self.resolution = 30
+        # Bounding box (WGS84)
+        self.BBox: List[float] = [-16.9, 13.6, -16.3, 14.2]
 
-        #Time interval of wanted imagery
-        self.timeInterval = "2019-06-01"
-        self.intervalTime = "2022-07-25"
-        
-        "KmlCSV()"
-        
-        # Give path to .csv file
-        # Be sure that columns' name are 'Name', 'Longitude' & 'Latitude'
-        # And that coordinates use wgs84 coordinate system
-        self.csvSrc = r''
-        # Or set path to .kml file to be converted to .csv file
-        self.kmlSrc = r'../Sine Saloum 2.0.kml'
-        
-        "PlotDATA()"
-        
-        # The figsize parameter takes a tuple of -
-        # the height and width of the plot layout
-        # Edit it, change the size of the annotations
-        self.figSize = (32.0, 24.0)
-        
-        # Increase DotPerInch for higher resolutions (& more processing time)
-        self.dpi = 100.0
-        
-        #Figure Title & Label (at the bottom of Fig)
-        self.figTitle = 'Le Sine Saloum'
-        self.xLabel = '05/08/2022'
-        
-        # Want Grid, else set to false
-        self.grid = True
-        
-        #If true, plot major & minor locator
-        self.locator = True
-        # Set major
-        self.xMajor = 0.05
-        self.yMajor = 0.05
-        # Set minor
-        self.xMinor = 4
-        self.yMinor = 4
-        
-        # Dirty Scale (Set to False if don't want it)
-        self.scale = True
-        self.xy = (-16.40, 13.6625)
-        self.xyText = (-16.3495, 13.6625)
-        self.arrowStyle = '<|-|>'
-        self.nomenclature = '5 500m'
-        self.xyNomenclature = (-16.3835, 13.664)
-        
-        # Name of city name whos will be moved a litlle upward
-        self.listyUp = ['Bambougar Massemba', 'Bangalere', 'Biogane', 'Mar Lodj',
-                   'Palmarin Ngethé', 'Soum']
-        # Name of city name whos will be moved a litlle downward
-        self.listyDown = ['Bambougar Malech', 'Bassar', 'Fayako', 'Diathanor', 'Gagué Mode',
-                     'Joal-Fadiout', 'Ndangane Sambou', 'Velingara']
-        
-        # Coordinates correction & color of "Name":
-        # To prevent the "Name" from becoming entangled on the map, -
-        # we move some of them a little upward, or downward  -
-        # (depend if in listyUp or listyDown).
-        
-        # UPPER names (could be river or island)
-        self.upperLonCorrection = -0.0005
-        self.upperLatCorrection = 0.001
-        self.upperColor = 'red'
-        # Upward names
-        self.upwardLonCorrection = -0.01
-        self.upwardLatCorrection = 0.001
-        self.upwardColor = 'black'
-        # Downward names
-        self.downwardLonCorrection = -0.013
-        self.downwardLatCorrection = -0.004
-        self.downwardColor = 'black'
-        # Normals ones
-        self.normalLonCorrection = 0
-        self.normalLatCorrection = 0.001
-        self.normalColor = 'black'
-        # ArrowColor
-        self.arrowColor = 'blue'        
-        
-        """Basic directory system."""
-        # Get the name of the current directory
-        currentDirectory = os.path.basename(os.getcwd())
-        # Check if already in the right directory
-        if currentDirectory != 'SentinelDownload':
-            # Will triger only once, while the working directory 
-            # will now be in the "SentinelDownload" directory
-            logging.warning('Start of SentinelHub downloading !')
+        # SentinelHub download parameters
+        self.resolution: int = 30  # meters/pixel
+        self.timeInterval: str = "2019-06-01"
+        self.intervalTime: str = "2022-07-25"
+
+        # KML/CSV sources
+        self.csvSrc: str = r""
+        self.kmlSrc: str = r"../Sine Saloum 2.0.kml"
+
+        # Plotting parameters
+        self.figSize: Tuple[float, float] = (32.0, 24.0)
+        self.dpi: float = 100.0
+        self.figTitle: str = "Le Sine Saloum"
+        self.xLabel: str = "05/08/2022"
+        self.grid: bool = True
+        self.locator: bool = True
+        self.xMajor: float = 0.05
+        self.yMajor: float = 0.05
+        self.xMinor: int = 4
+        self.yMinor: int = 4
+
+        # Scale bar
+        self.scale: bool = True
+        self.xy: Tuple[float, float] = (-16.40, 13.6625)
+        self.xyText: Tuple[float, float] = (-16.3495, 13.6625)
+        self.arrowStyle: str = "<|-|>"
+        self.nomenclature: str = "5 500m"
+        self.xyNomenclature: Tuple[float, float] = (-16.3835, 13.664)
+
+        # Label positioning lists
+        self.listyUp: List[str] = [
+            "Bambougar Massemba",
+            "Bangalere",
+            "Biogane",
+            "Mar Lodj",
+            "Palmarin Ngethé",
+            "Soum",
+        ]
+        self.listyDown: List[str] = [
+            "Bambougar Malech",
+            "Bassar",
+            "Fayako",
+            "Diathanor",
+            "Gagué Mode",
+            "Joal-Fadiout",
+            "Ndangane Sambou",
+            "Velingara",
+        ]
+
+        # Label corrections and colors
+        self.upperLonCorrection: float = -0.0005
+        self.upperLatCorrection: float = 0.001
+        self.upperColor: str = "red"
+
+        self.upwardLonCorrection: float = -0.01
+        self.upwardLatCorrection: float = 0.001
+        self.upwardColor: str = "black"
+
+        self.downwardLonCorrection: float = -0.013
+        self.downwardLatCorrection: float = -0.004
+        self.downwardColor: str = "black"
+
+        self.normalLonCorrection: float = 0.0
+        self.normalLatCorrection: float = 0.001
+        self.normalColor: str = "black"
+
+        self.arrowColor: str = "blue"
+
+        # Working directory setup
+        self.workingDirectory: str = self._init_working_directory()
+
+    def _init_working_directory(self) -> str:
+        """Ensure that 'SentinelDownload' directory exists and switch to it."""
+        current_directory = os.path.basename(os.getcwd())
+        if current_directory != "SentinelDownload":
+            logging.warning("Start of SentinelHub downloading !")
             try:
-                # Create directory
-                os.mkdir('SentinelDownload')
-                logging.debug('Directory "SentinelDownload" succesfully created !')
+                os.mkdir("SentinelDownload")
+                logging.debug('Directory "SentinelDownload" successfully created!')
             except FileExistsError:
-                logging.debug('Directory "SentinelDownload" already exist !')
+                logging.debug('Directory "SentinelDownload" already exists!')
             finally:
-                # Move in
-                os.chdir('SentinelDownload/')
-        # Update the working directory
-        self.workingDirectory = os.getcwd()
+                os.chdir("SentinelDownload/")
+        return os.getcwd()
 
 
-#
-if __name__ == '__main__':
-    # Trigger the executions sequences.
+if __name__ == "__main__":
     try:
-        # Import other class()
         from SentinelHubDownload import SentinelHubDownload
         from PlotDATA import PlotDATA
-        # Trigger the execution Sequence of SentinelHubDownload()
-        exeSeq = SentinelHubDownload()
-        exeSeq.exeSeq()
-        # Trigger the execution Sequence of PlotData()
-        exeSeq = PlotDATA()
-        exeSeq.exeSeq()
+
+        exe_seq = SentinelHubDownload()
+        exe_seq.exeSeq()
+
+        exe_seq = PlotDATA()
+        exe_seq.exeSeq()
     except Exception as e:
         print(e)
